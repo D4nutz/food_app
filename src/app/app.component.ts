c6b6220bec19d424f88f6299dc3c59cbee4904b3
D4nutz <manciudanielvlog1@gmail.com>
2024-11-23 14:07:29 +0200

    implement navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,29 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomepageComponent } from '../assets/views/homepage/homepage.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HomepageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'food_app';
+  isMobile:boolean = false;
+
 }
+
+function isMobile() {
+  const regex = /Mobi|Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i;
+  return regex.test(navigator.userAgent);
+}
+
+if (isMobile()) {
+  console.log("Mobile device detected");
+} else {
+  console.log("Desktop device detected");
+}
+
+
