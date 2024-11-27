00cdbe1dca2c04eb22bab81f20b6e7a435779580
D4nutz <manciudanielvlog1@gmail.com>
2024-11-27 21:35:50 +0200

    adding icons and search-bar

--- src/assets/widgets/navbar/navbar.component.ts
+++ src/assets/widgets/navbar/navbar.component.ts
@@ -1,10 +1,12 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';  
+import { SearchBarComponent } from '../search-bar/search-bar.component';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, SearchBarComponent, MatIconModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
