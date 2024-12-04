3b8876ccae9b18dab771a7834b16e3666f1cc7c9
D4nutz <manciudanielvlog1@gmail.com>
2024-12-04 20:38:29 +0200

    add new view

--- src/assets/views/homepage/homepage.component.ts
+++ src/assets/views/homepage/homepage.component.ts
@@ -1,11 +1,10 @@
 import { Component, Input } from '@angular/core';
 import { NavbarComponent } from '../../widgets/navbar/navbar.component';
-import { UserFlowerSelectorComponent } from '../../widgets/user-flower-selector/user-flower-selector.component';
 
 @Component({
   selector: 'app-homepage',
   standalone: true,
-  imports: [NavbarComponent, UserFlowerSelectorComponent],
+  imports: [NavbarComponent],
   templateUrl: './homepage.component.html',
   styleUrl: './homepage.component.scss'
 })
--- src/assets/widgets/navbar/navbar.component.ts
+++ src/assets/widgets/navbar/navbar.component.ts
@@ -2,11 +2,12 @@ import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';  
 import { SearchBarComponent } from '../search-bar/search-bar.component';
 import { MatIconModule } from '@angular/material/icon';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [CommonModule, SearchBarComponent, MatIconModule],
+  imports: [CommonModule, RouterModule, SearchBarComponent, MatIconModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
